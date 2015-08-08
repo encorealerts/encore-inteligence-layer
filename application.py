285561d13a30d54710c4ab47c0a621e596daa1c6
felipeclopes <felipelopes10@gmail.com>
2015-08-08 08:46:46 -0700

    added: First version of flask service with cherrypy

---
+++ application.py
@@ -0,0 +1,27 @@
+from actor_classification import ActorClassification
+
+from flask import Flask, jsonify, request
+
+import traceback
+
+resources = {}
+def create_app():
+  app = Flask(__name__)
+  
+  resources['actor_classification'] = ActorClassification()
+
+  return app
+
+app = create_app()
+
+@app.route("/<modelname>/predict", methods=['POST', 'OPTIONS'])
+def predict(modelname):
+	try:
+		json_request = request.get_json(force=True)
+		return jsonify(resources[modelname].predict(json_request))
+	except Exception as e:
+		traceback.print_exc()
+
+
+if __name__ == "__main__":
+    app.run()
