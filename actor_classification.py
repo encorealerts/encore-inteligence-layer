38c36919eeebe47a1e02adc364327418ab6646e7
Daniel Adornes <adornes@Daniels-MacBook-Pro.local>
2015-11-19 18:34:47 -0200

    new actor classification

--- actor_classification.py
+++ actor_classification.py
@@ -12,24 +12,11 @@ from sklearn.externals import joblib
 from datetime import timedelta
 
 from dateutil import relativedelta
-from dateutil import parser
 from datetime import datetime
 
 import numpy as np
 
 class ActorClassification:
-  manual_generators = ['Twitter Web Client', 
-                     'Twitter for iPhone', 
-                     'Twitter for Android', 
-                     'Twitter for BlackBerry', 
-                     'Twitter for Windows Phone', 
-                     'Twitter for iPad', 
-                     'Twitter for BlackBerry\xc2\xae', 
-                     'Twitter for Mac', 
-                     'Twitter for Android Tablets', 
-                     'Twitter for Windows', 
-                     'Twitter for Apple Watch', 
-                     'Twitter for  Android']
 
   def __init__(self):
     self.load()
@@ -48,63 +35,92 @@ class ActorClassification:
     forest_path = forest_path[-1]
     self.model = joblib.load(forest_path)
 
-    counter_path = sorted(glob.glob('../encore-luigi/data/actor_classification/deploy/bio_count_vectorizer_*.pkl'))
-    counter_path += sorted(glob.glob('/mnt/encore-luigi/data/actor_classification/deploy/bio_count_vectorizer_*.pkl'))    
-    counter_path = counter_path[-1]
-    vocabulary = joblib.load(counter_path) 
-    self.counter = CountVectorizer(tokenizer=self.tokenize, ngram_range=(1,3), vocabulary = vocabulary)
+    model_features_path = sorted(glob.glob('../encore-luigi/data/actor_classification/deploy/actor_classification_random_forest_features_*.pkl'))
+    model_features_path += sorted(glob.glob('/mnt/encore-luigi/data/actor_classification/deploy/actor_classification_random_forest_features_*.pkl'))    
+    model_features_path = model_features_path[-1]
+    self.model_features = joblib.load(model_features_path)
 
-  def tokenize(self, text):
-    tokenizer = nltk.RegexpTokenizer(r'\w+')
-    tokens = tokenizer.tokenize(text)
-    tokens = [('NUM' if word.isdigit() else word) for word in tokens]
-    return tokens
+  def perform_feature_engineering(self, data):
+    # Remove non-relevant columns
+    data = data.drop(["segment"], axis=1)
+    data = data.drop(["link"], axis=1)
+
+    # Transform boolean 'verified' to 0/1
+    data.ix[data.verified.isnull(), 'verified'] = False
+    data.ix[data.verified == True,  'verified'] = 1
+    data.ix[data.verified == False, 'verified'] = 0
+
+    # OneHotEncoding for 'lang'
+    if "lang" in data:
+      data.ix[(data.lang == 'Select Language...') | (data.lang.isnull()), 'lang'] = None
+      for lang in list(set(data.lang)):
+        if lang != None:
+          data.ix[data.lang == lang, "lang_"+lang] = 1
+          data.ix[data.lang != lang, "lang_"+lang] = 0
+      data = data.drop(["lang"], axis=1)
+
+    # Treat special characters
+    text_fields = ["name", "screen_name","summary"]
+
+    def treat_special_char(c):
+      try:
+        return '0' if c.isdigit() else c.decode().encode("utf-8")
+      except UnicodeDecodeError:
+        return '9'
+
+    for field in text_fields:
+      data.ix[data[field].isnull(), field] = "null"
+      data[field] = map(lambda n: ''.join(map(lambda c: treat_special_char(c), list(n))), data[field].values)
+
+    # CountVectorizer for 'screen_name' and 'name'
+    def num_char_tokenizer(text):
+      return list(text)
+
+    for field in ["screen_name","name"]:
+      if field in data:
+        field_countvect = CountVectorizer(tokenizer=num_char_tokenizer,
+                                          ngram_range=(3, 5), 
+                                          analyzer="char",
+                                          min_df = 8,
+                                          vocabulary = filter(lambda f: f.startswith(field+"_"), 
+                                                              self.model_features))
+
+        field_matrix = field_countvect.fit_transform(data[field])
+        features_names = map(lambda f: "_".join([field,f]), field_countvect.get_feature_names())
+        field_df = pd.DataFrame(field_matrix.A, columns=features_names)
+
+        data = pd.concat([data, field_df], axis=1, join='inner').drop([field], axis=1)
+
+    # CountVectorizer for 'summary'
+    def num_word_tokenizer(text):
+      tokenizer = nltk.RegexpTokenizer(r'\w+')
+      return tokenizer.tokenize(text)
+
+    if "summary" in data:
+      summary_countvect = CountVectorizer(tokenizer=num_word_tokenizer,
+                                          ngram_range=(2, 4), 
+                                          analyzer="word",
+                                          min_df = 5,
+                                          vocabulary = filter(lambda f: f.startswith("summary_"), 
+                                                              self.model_features))
+
+      summary_matrix = summary_countvect.fit_transform(data.summary)
+      features_names = map(lambda f: "_".join(["summary",f]), summary_countvect.get_feature_names())
+      summary_df = pd.DataFrame(summary_matrix.A, columns=features_names)
+      data = pd.concat([data, summary_df], axis=1, join='inner').drop(["summary"], axis=1)
+
+    # Treat remaining null values
+    data = data.fillna(0)
+
+    return data   
 
   def predict(self, raw_data):
-    actor_summary = raw_data.get('actor_summary', '')
-    actor_summary = '' if actor_summary is None else actor_summary
-    transformed = self.counter.transform([actor_summary])
-    transformed_features = transformed.toarray()[0]
-
-    actor_verified = int(raw_data['actor_verified'])
-
-    actor_followers_count = int(1 if raw_data['actor_followers_count'] is None else raw_data['actor_followers_count'])
-    actor_friends_count = int(1 if raw_data['actor_friends_count'] is None else raw_data['actor_friends_count'])
-    actor_favorites_count = int(1 if raw_data['actor_favorites_count'] is None else raw_data['actor_favorites_count'])
-    actor_statuses_count = int(1 if raw_data['actor_statuses_count'] is None else raw_data['actor_statuses_count'])
-    actor_listed_count = int(1 if raw_data['actor_listed_count'] is None else raw_data['actor_listed_count'])
-
-    manually_tweeting = int(raw_data['tweet_generator'] in self.manual_generators)
-    registration_from_now = self.actor_registration_from_now(raw_data['actor_created_at'])
-    followers_friends_ratio = actor_followers_count/float(actor_friends_count)
-    favourites_friends_ratio = actor_favorites_count/float(actor_friends_count)
-    favourites_followers_ratio = actor_favorites_count/float(actor_followers_count)
-    favourites_status_ratio = actor_favorites_count/float(actor_statuses_count)
-
-    calculated_features = [ 
-        actor_favorites_count, 
-        actor_followers_count, 
-        actor_friends_count, 
-        actor_listed_count, 
-        registration_from_now, 
-        actor_statuses_count, 
-        actor_verified, 
-        favourites_followers_ratio, 
-        favourites_friends_ratio,
-        favourites_status_ratio,
-        followers_friends_ratio, 
-        manually_tweeting
-    ]
-    all_features = np.concatenate([calculated_features, transformed_features])
+    df = pd.DataFrame([pd.Series(raw_data)])
+    df = self.perform_feature_engineering(df)
     
-    result = self.model.predict_proba(all_features)[0]
+    result = self.model.predict_proba(df)[0]
 
     return {
       'business': result[0],
       'person': result[1]
     }
-
-  def actor_registration_from_now(self, registration):
-      r = parser.parse(registration)
-      d = relativedelta.relativedelta(datetime.now(), r)
-      return d.years * 12 + d.months
