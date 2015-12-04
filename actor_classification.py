9d9c9e16a6a62abc0c3a44f4d26cea651d0f937e
Daniel Adornes <adornes@Daniels-MacBook-Pro.local>
2015-12-04 01:10:44 -0200

    Huge refactoring on Luigi vs Inteligence Layer approach

--- actor_classification.py
+++ actor_classification.py
@@ -20,15 +20,14 @@ class ActorClassification:
   def load(self):
     self.last_loaded = datetime.now()
 
-    model_path = sorted(glob.glob('../encore-luigi/data/actor_classification/deploy/actor_classification_random_forest_2*.pkl'))
-    model_path += sorted(glob.glob('/mnt/encore-luigi/data/actor_classification/deploy/actor_classification_random_forest_2*.pkl'))
+    model_path = sorted(glob.glob('../encore-luigi/data/actor_classification/deploy/actor_classification_trained_model_2*.pkl'))
+    model_path += sorted(glob.glob('/mnt/encore-luigi/data/actor_classification/deploy/actor_classification_trained_model_2*.pkl'))
 
     model_path = model_path[-1]
     self.model = joblib.load(model_path)
 
   def predict(self, raw_data):
     df = pd.DataFrame([pd.Series(raw_data)])
-    df = self.perform_feature_engineering(df)
     
     result = self.model.predict_proba(df)[0]
 
