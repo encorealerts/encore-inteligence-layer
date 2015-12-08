fefbc71611d95dda226e2b7f4bfa356f0a5cf0b1
Daniel Adornes <adornes@Daniels-MacBook-Pro.local>
2015-12-08 02:24:54 -0200

    Changing strategy to host the service

--- application.py
+++ application.py
@@ -1,8 +1,17 @@
+import sys
+import cherrypy as cp
+from cherrypy.wsgiserver import CherryPyWSGIServer
+from cherrypy.process.servers import ServerAdapter
+
 from actor_classification import *
 
 from flask import Flask, jsonify, request
 
 import traceback
+
+# =========================
+# Flask application 
+# =========================
 
 resources = {}
 def create_app():
@@ -26,6 +35,40 @@ def predict(modelname):
 	except Exception as e:
 		traceback.print_exc()
 
+# =========================
+# Hosting 
+# =========================
+
+def run_decoupled(app, port, host='0.0.0.0', **config):
+  server = CherryPyWSGIServer((host, port), app, **config)
+  try:
+      server.start()
+  except KeyboardInterrupt:
+      server.stop()
+
+def run_in_cp_tree(app, port, host='0.0.0.0', **config):
+  cp.tree.graft(app, '/')
+  cp.config.update(config)
+  cp.config.update({
+      'server.socket_port': port,
+      'server.socket_host': host
+  })
+  cp.engine.signals.subscribe() # optional
+  cp.engine.start()
+  cp.engine.block()
+
+def run_with_adapter(app, port, host='0.0.0.0', config=None, **kwargs):
+  cp.server.unsubscribe()
+  bind_addr = (host, port)
+  cp.server = ServerAdapter(cp.engine,
+                            CherryPyWSGIServer(bind_addr, app, **kwargs),
+                            bind_addr).subscribe()
+  if config:
+    cp.config.update(config)
+  cp.engine.signals.subscribe() # optional
+  cp.engine.start()
+  cp.engine.block()
 
 if __name__ == "__main__":
-    app.run()
+  port = (5001 if len(sys.argv) == 1 else int(sys.argv[1]))
+  run_with_adapter(app, port=port)
